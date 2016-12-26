dad3ffac28b67d8ced455589bb0d395d3e9a1400
fibo <casati_gianluca@yahoo.it>
2016-12-26 03:17:12 +0100

    resizable

--- src/PlaneView.js
+++ src/PlaneView.js
@@ -1,16 +1,91 @@
-// TODO first goal is to make canvas
-// responsive and resizable
-function PlaneView () {
-  this.id = 'plane-view'
-  this.canvas = null
-  this.context = null
-  this.fillStyle = 'rgb(200,0,0)'
-  this.size = 256
-  this.unit = this.size / 16
+var staticProps = require('static-props')
+
+function PlaneView (cells) {
+  var id = 'plane-view'
+
+  function getSize () {
+    return Math.min(window.innerHeight, window.innerWidth)
+  }
+
+  var canvas = document.getElementById(id)
+  var context = canvas.getContext('2d')
+
+  function getUnit () {
+    var border = 1
+    var size = getSize()
+    return (size - 2 * border) / 16
+  }
+
+  function setCanvasSize () {
+    var size = getSize()
+    canvas.height = size
+    canvas.width = size
+  }
+
+  setCanvasSize()
+
+  staticProps(this)({
+    cells: cells,
+    context: context,
+    id: id,
+    size: getSize,
+    unit: getUnit
+  })
+
+  // Get a binded version of draw...
+  var _draw = draw.bind(this)
+  // ...use to draw on start up...
+  _draw()
+
+  window.addEventListener('resize', function () {
+    setCanvasSize()
+    // ...and to redraw on resize.
+    _draw()
+  })
 }
 
-function drawCells (cells) {
+function draw () {
+  this.context.translate(0.5, 0.5)
+
+  // Incredible, it works! But how?
+  // Where do the following functions take the `this` context!?
+  // De Mysteriis Dom JavaScript
+  // https://www.youtube.com/watch?v=qcS0CVJ1KPg
+  drawCells()
+  drawGrid()
+}
+
+PlaneView.prototype.draw = draw
+
+function drawGrid () {
   var context = this.context
+  var size = this.size
+  var unit = this.unit
+
+  context.lineWidth = 1
+
+  for (var i = 0; i <= size; i += unit) {
+    context.strokeStyle = '#aeaeae'
+
+    context.beginPath()
+    context.moveTo(i, 0)
+    context.lineTo(i, size)
+    context.stroke()
+
+    context.beginPath()
+    context.moveTo(0, i)
+    context.lineTo(size, i)
+    context.stroke()
+  }
+}
+
+PlaneView.prototype.drawGrid = drawGrid
+
+function drawCells () {
+  var cells = this.cells
+  var context = this.context
+  context.fillStyle = 'rgb(200,0,0)'
+
   var unit = this.unit
 
   for (var i = 0; i < 16; i++) {
@@ -24,19 +99,5 @@ function drawCells (cells) {
 }
 
 PlaneView.prototype.drawCells = drawCells
-
-function init () {
-  var id = this.id
-  var size = this.size
-
-  var canvas = this.canvas = document.getElementById(id)
-  canvas.height = size
-  canvas.width = size
-
-  var context = this.context = canvas.getContext('2d')
-  context.fillStyle = this.fillStyle
-}
-
-PlaneView.prototype.init = init
 
 module.exports = PlaneView
